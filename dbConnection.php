68cd7eba8c6622db9f62c0aa55836ba74a7f3340
davevon <61807777+davevon@users.noreply.github.com>
2022-10-30 15:26:35 -0500

    setting up database connection

---
+++ dbConnection.php
@@ -0,0 +1,23 @@
+<?php  
+
+$db_host = "localhost";
+$db_user = "root";
+$db_pssword = "";
+$db_name = "oms_database";
+
+
+//create database
+
+$conn = new mysqli($db_host, $db_user, $db_pssword, $db_name);
+
+// check if database exists
+
+if($conn->connect_error)
+{
+    die("Error connection");
+
+} else{
+    echo "Database connection established";
+}
+
+?>
