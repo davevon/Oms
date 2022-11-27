2a61975985cabc5b7d01f9852a91613d3536291e
davevon <61807777+davevon@users.noreply.github.com>
2022-11-27 18:15:52 -0500

    added technician details

--- Admin/Technician.php
+++ Admin/Technician.php
@@ -1,8 +1,87 @@
  <?php
+    session_start();
     define('TITLE', 'Technicians');
     define('PAGE', 'Technicians');
     include('../dbConnection.php');
     include('includes/header.php');
+
+
+    if (isset($_SESSION['is_adminlogin'])) {
+        $aEmail = $_SESSION['aEmail'];
+    } else {
+        echo "<script>
+     location.href = 'login.php';
+ </script>";
+    }
     ?>
 
-<?php include('includes/footer.php') ?>
+ <div class="col-sm-9 col-md-10 mt-5 text-center">
+     <p class="bg-dark text-center text-white p-2"> List of Technicians</p>
+
+     <?php
+        $sql = "SELECT * FROM technician_tb";
+        $result = $conn->query($sql);
+        if ($result->num_rows > 0) {
+            echo '<table class="table">';
+            echo '<thead>';
+            echo '<tr>';
+            echo '<th scope="col">Emp ID</th>';
+            echo '<th scope=" col">Emp Name</th>';
+            echo '<th scope=" col">Emp City</th>';
+            echo '<th scope=" col">Emp Mobile</th>';
+            echo '<th scope=" col">Email</th>';
+            echo '<th scope=" col">Status</th>';
+            echo '<tr>';
+            echo '</thead';
+            echo '<tbody>';
+            while ($row = $result->fetch_assoc()) {
+                echo '<tr>';
+                echo '<td>' . $row["empid"] . '</td>';
+                echo '<td>' . $row['empName'] . '</td>';
+                echo '<td>' . $row['empCity'] . '</td>';
+                echo '<td>' . $row['empMobile'] . '</td>';
+                echo '<td>' . $row['empEmail'] . '</td>';
+                echo '<td>';
+                echo '<form action="editremp.php" class="d-inline" method="POST" >';
+                echo '<input type="hidden" name="id" value=' . $row["empid"] . '><button type="submit" class="btn btn-info mr-3" name="edit" value="Edit"><i class="fas fa-pen"></i></button>';
+                echo '</form>';
+                echo '<form action="" class="d-inline" method="POST" >';
+                echo '<input type="hidden" name="id" value=' . $row["empid"] . '><button type="submit" class="btn btn-secondary mr-3" name="delete" value="Delete"><i class="fas fa-trash"></i></button>';
+                echo '</form>';
+                echo '</td>';
+                echo '</tr>';
+            }
+            echo '</tbody>';
+
+            echo '</table>';
+        } else {
+            echo '0 Results';
+        }
+        ?>
+ </div>
+ <?php
+    if (isset($_REQUEST['delete'])) {
+        $sql = "DELETE FROM technician_tb where empid = {$_REQUEST['id']}";
+        if ($conn->query($sql) == true) {
+            echo '<meta http-equiv="refresh" content="0;URL=?deleted" />';
+        } else {
+            echo 'Unable to delete';
+        }
+    } ?>
+
+
+ </div><!-- End row  -->
+ <div class="float-right"> <a href="insertemp.php" class="btn btn-success"><i class="fas fa-plus fa-2x"></i></a>
+ </div>
+
+
+ </div> <!-- End Container  -->
+
+ <!-- JavaScript -->
+ <script src="../js/Jquery.min.js"></script>
+ <script src="../js/popper.min.js"></script>
+ <script src="../js/all.min.js"></script>
+ <script src="../js/bootstrap.min.js"></script>
+ </body>
+
+ </html>
---
+++ Admin/insertemp.php
@@ -0,0 +1,82 @@
+<?php
+session_start();
+define('TITLE', 'Update Technician');
+define('PAGE', 'Technicians');
+include('../dbConnection.php');
+include('includes/header.php');
+if (isset($_SESSION['is_adminlogin'])) {
+    $aEmail = $_SESSION['aEmail'];
+} else {
+    echo "<script>location.href='login.php';</script>";
+}
+if (isset($_REQUEST['empsubmit'])) {
+    if (($_REQUEST['empName'] == "") || 
+     ($_REQUEST['empCity'] == "") 
+    
+        || ($_REQUEST['empMobile'] == "")
+          || ($_REQUEST['empEmail'] == "")
+    ) {
+        $msg = '<div class="alert alert-warning col-sm-6 ml-5 mt-2" role="alert" >Fill All Fields </div>';
+    } else {
+        $eName = $_REQUEST['empName'];
+        $eCity = $_REQUEST['empCity'];
+        $eMobile= $_REQUEST['empMobile'];
+        $eEmail = $_REQUEST['empEmail'];
+        $sql = "INSERT INTO technician_tb (empName, empCity, empMobile, empEmail ) VALUES ('$eName','$eCity','$eMobile','$eEmail')";
+        if ($conn->query($sql) == TRUE) {
+            $msg = '<div class="alert alert-success col-sm-6 ml-5 mt-2" role="alert">Added Successfully</div>';
+        } else {
+            '<div class="alert alert-danger col-sm-6 ml-5 mt -2" role="alert">Unable to Add</div>';
+        }
+    }
+}
+?>
+<!-- Start 2nd column -->
+
+<div class="col-sm-6 mt-5 mx-3 jumbotron">
+    <h3 class="text-center">Add new Technician</h3>
+    <form action="" method="POST">
+        <div class="form-group">
+            <label for="empName">Name</label>
+            <input type="text" class="form-control" id="empName" name="empName">
+        </div>
+        <div class="form-group">
+            <label for="empEmail">Email</label>
+            <input type="email" class="form-control" id="empEmail" name="empEmail">
+
+        </div>
+        <div class="form-group">
+            <label for="empCity">City</label>
+            <input type="text" class="form-control" id="empCity" name="empCity">
+
+        </div>
+        <div class="form-group">
+            <label for="empMobile">Mobile</label>
+            <input type="text" class="form-control" id="empMobile" name="empMobile" onkeypress="isInputNumber(event)">
+
+        </div>
+        <div class="text-center">
+            <button type="submit" class="btn btn-danger" id="empsubmit" name="empsubmit">Submit</button>
+            <a href="technician.php" class="btn btn-secondary">Close</a>
+
+        </div>
+        <?php if (isset($msg)) {
+            echo $msg;
+        } ?>
+
+    </form>
+</div>
+<!-- Only Number for Input Fields -->
+<script>
+    function isInputnumber(evt) {
+        var ch = String.fromCharCode(evt.which);
+        if (!(/[0-9])/.test(ch))) {
+            evt.preventDefault();
+        }
+    }
+</script>
+<!-- End Send column -->
+<?php
+include('includes/footer.php');
+
+?>;
